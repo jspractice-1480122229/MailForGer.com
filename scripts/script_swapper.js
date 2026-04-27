7ab21094d21a4829ddee4ac7ebc30938412640c2
Margi Naverra <jspractice-1480122229@users.noreply.github.com>
2026-04-26 23:16:06 -0700

    refactor(index.html, manifest.json, script_swapper.js): update and clean up meta tags, add site name to manifest, and use const for random number generation

--- scripts/script_swapper.js
+++ scripts/script_swapper.js
@@ -1,6 +1,6 @@
 // Randomly load one of five themes (site0–site4)
-var rando = Math.floor(Math.random() * 5);
-var link = document.createElement('link');
+const rando = Math.floor(Math.random() * 5);
+const link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = 'css/site' + rando + '.css';
