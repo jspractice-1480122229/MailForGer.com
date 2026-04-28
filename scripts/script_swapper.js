907d9dbf1abb3f0f26c10e3c9c3be2259aa375eb
Margi Naverra <jspractice-1480122229@users.noreply.github.com>
2026-04-28 00:41:56 -0700

    fix: render-blocking stylesheet via document.write to prevent FOUC

--- scripts/script_swapper.js
+++ scripts/script_swapper.js
@@ -1,7 +1,2 @@
-// Randomly load one of five themes (site0–site4)
 const rando = Math.floor(Math.random() * 5);
-const link = document.createElement('link');
-link.rel = 'stylesheet';
-link.type = 'text/css';
-link.href = 'css/site' + rando + '.css';
-document.head.appendChild(link);
+document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/site" + rando + ".css\">");
